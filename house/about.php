9798cbc909049a5bca7297e0b3a95031c09682e1
Dos Santos Hindjou <Dozzy7@yahoo.com>
2017-06-20 09:55:33 +0800

    added this page

--- house/about.php
+++ house/about.php
@@ -18,13 +18,29 @@ require 'includes/db.connection.php';
 
     <!----------------  Body ---------------->
     <section id="about" class="text-center">
-        <h2>  We’re the UK’s largest property portal.  </h2>
-    <article>
-        Our aim is to empower the Chinas’s decisions around property. 
-        Our sophisticated, yet simple, property search platform 
+        <div class="col-sm-6 imageC">
+            <h2>  We are Africas largest property portal.  </h2>
+        </div>
+        <div class="col-sm-6">
+            
+    <article><br><br>
+        Our aim is to empower the Chinas’s decisions around property. <br>
+        Our sophisticated, yet simple, property search platform <br>
         makes it easy for home hunters to find their ‘happy’.<br>
-        Raydines plc floated on the London Stock Exchange on 15 March 2006.
+        Raydines plc floated on the Aficas Stock Exchange on 15 March 2013.<br><br>
     </article>
+    <article>
+        <h3>Welcome to Rayline Housing Agency</h3>
+        We are the fastest growing company in the online real estate space.<br>
+         Born out of the need to simplify the search for a home, <br>
+         free of fake listings and endless site visits,<br>
+          we created a unique property search platform that filled the gaps<br>
+           left by others in the market. Led by passionate problem-solvers,<br>
+            and backed by top investors from around the world,<br>
+             we are poised to become the most trusted place to find a home in Africa.
+    </article>
+        </div>
+        
  </section>
 
 
